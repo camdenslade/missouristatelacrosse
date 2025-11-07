1c740d81845a211f2950bc463661522147e21421
camdenslade <157507107+camdenslade@users.noreply.github.com>
2025-11-07 10:36:23 -0600

    Switched to EC2 instance

--- backend/.platform/hooks/predeploy/01_expose_port.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "PORT=8080" > /etc/profile.d/port.sh
--- backend/.platform/hooks/predeploy/02_fix_logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if systemctl list-unit-files | grep -q eb-docker-log.service; then
-  systemctl enable eb-docker-log
-  systemctl restart eb-docker-log
-fi
